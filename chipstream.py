09497cbc2b90633c6211f458379f20c13347d289
Maros Kuzmiak <kuzmiak.maros@gmail.com>
2019-08-25 22:08:20 +0100

    ok

--- chipstream.py
+++ chipstream.py
@@ -1,24 +1,56 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-
+ 
 import creds
+ 
+# # # # TWITTER STREAMER # # # #
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+    def __init__(self):
+        pass
 
-class StdOutLinstener(StreamListener):
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authetification and the connection to Twitter Streaming API
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(creds.consumer_key, creds.consumer_secret)
+        auth.set_access_token(creds.access_token, creds.access_token_secret)
+        stream = Stream(auth, listener)
+
+        # This line filter Twitter Streams to capture data by the keywords: 
+        stream.filter(track=hash_tag_list)
+
+
+# # # # TWITTER STREAM LISTENER # # # #
+class StdOutListener(StreamListener):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
 
     def on_data(self, data):
-        print(data)
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
         return True
+          
 
-    def on_error(self,status):
+    def on_error(self, status):
         print(status)
 
-if __name__ == "__main__":
+ 
+if __name__ == '__main__':
+ 
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["maros", "kuzmiak"]
+    fetched_tweets_filename = "tweets.txt"
 
-    listener = StdOutLinstener()
-    auth = OAuthHandler(creds.consumer_key, creds.consumer_secret)
-    auth.set_access_token(creds.access_token,creds.access_token_secret)
-
-    stream = Stream(auth, listener)
-
-    stream.filter(track=['amazonia','notredame'])
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
