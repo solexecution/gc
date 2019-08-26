953e887de9afd614b643b8c63fa774c2d385a5c7
Maros <solexecution@gmail.com>
2019-08-26 23:12:40 +0100

    ok

--- ok.py
+++ ok.py
@@ -9,14 +9,20 @@ using AppAuthHandler to go faster as we need read-only access to public informat
 auth = tweepy.AppAuthHandler(creds.consumer_key,creds.consumer_secret) 
 api = tweepy.API(auth)
 
-# twitter_accounts = ['@get_chip','@monzo','@StarlingBank','@RevolutApp']
+twitter_accounts = ['@get_chip','@monzo','@StarlingBank','@RevolutApp']#,'#get_chip','#monzo','#StarlingBank','#RevolutApp']
 
-account_tweets = tweepy.Cursor(api.search, q='revolut'+'-filter:retweets').items(10)
+#  https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object.html
+def collect_account_tweets(account_iterable,multifile=True):
+    tweet_collection = [tweet._json for tweet in account_iterable]
+    if(multifile):
+        with open(time.strftime("%H%M%S_%d%m%Y_")+'tweets_'+str(len(tweet_collection))+'.json', 'at') as f:
+            json.dump(tweet_collection, f)
+            f.close()
+    else:
+        with open('tweets.json', 'at') as f:    
+            json.dump(tweet_collection, f)
+            f.close()
 
-tweet_collection = [tweet.id for tweet in account_tweets]
-
-cnt = len(tweet_collection)
-
-with open('tweets_'+str(cnt)+time.strftime("_%d%m%Y-%H%M%S_")+'.json', 'at') as f:
-   json.dump(tweet_collection, f)
-   f.close()
+for account in twitter_accounts:
+    account_tweets = tweepy.Cursor(api.search, q=account+'-filter:retweets').items(2)
+    collect_account_tweets(account_tweets,multifile=False)
