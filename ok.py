f08fbc74472c3fc4a6bd31d25b9b9c07c71e2360
Maros Kuzmiak <kuzmiak.maros@gmail.com>
2019-08-26 16:44:07 +0100

    ok

--- ok.py
+++ ok.py
@@ -1,8 +1,18 @@
-import os
-import glob
+import tweepy 
+import creds # credentials for authorization
+
+""""
+using AppAuthHandler to go faster as we need read-only access to public information
+"""
+auth = tweepy.AppAuthHandler(creds.consumer_key,creds.consumer_secret) 
+api = tweepy.API(auth)
+
+# twitter_accounts = ['@get_chip','@monzo','@StarlingBank','@RevolutApp']
 
 
-def ok():
-    [os.path.realpath for p in glob.glob(p)]
+account_tweets = tweepy.Cursor(api.search, q='revolut'+'-filter:retweets').items(1000)
 
-ok()
+for tweet in account_tweets:
+    print(tweet._json)
+
+# for tweet in tweepy.Cursor(api.search, q='@get_chip'+'-filter:retweets').items(100):
